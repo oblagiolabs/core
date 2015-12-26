8f339a69586a0c01baf2ebb4d3fbafb1da808b32
unknown <reza.wikrama3@gmail.com>
2015-12-26 01:22:55 -0800

    Memberi Keterangan pada Setting.php

--- Packages/Oblagio/Setting.php
+++ Packages/Oblagio/Setting.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Konfigurasi
+|--------------------------------------------------------------------------
+|
+| Variable statusProject -> jika isinya dev maka menu developer pada aplikasi tetap ada jika isinya live maka menu developer hilang
+| Variable Backend -> variable yang berisi path uri backenend itu sendiri. jika variable backendName diubah maka uri nya pun berubah.
+|
+*/
+
 function oblagioSetting()
 
 {
