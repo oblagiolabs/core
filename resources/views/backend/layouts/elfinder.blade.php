3012142f7079382a856493010af2171158cee847
unknown <reza.wikrama3@gmail.com>
2015-12-26 14:41:27 -0800

    view elfinder

---
+++ resources/views/backend/layouts/elfinder.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Media Library</title>
+	<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+
+		<!-- elFinder CSS (REQUIRED) -->
+		<link rel="stylesheet" type="text/css" href="{{ helper()->elfinder() }}css/elfinder.min.css">
+		<link rel="stylesheet" type="text/css" href="{{ helper()->elfinder() }}css/theme.css">
+
+		<!-- elFinder JS (REQUIRED) -->
+		<script src="{{ helper()->elfinder() }}js/elfinder.min.js"></script>
+
+	
+		<!-- elFinder initialization (REQUIRED) -->
+		<script type="text/javascript" charset="utf-8">
+			// Documentation for client options:
+			// https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
+			$(document).ready(function() {
+				$('#elfinder').elfinder({
+					url : '{{ helper()->elfinder() }}php/connector.minimal.php'  // connector URL (REQUIRED)
+					// , lang: 'ru'                    // language (OPTIONAL)
+				});
+			});
+		</script>
+    
+</head>
+<body>
+<div id = 'elfinder'>
+
+</div>
+</body>
+</html>
---
+++ resources/views/backend/media/index.blade.php
@@ -0,0 +1,13 @@
+@extends('backend.layouts.layout')
+
+@section('content')
+		
+	<div class="row">
+		<div class="col-md-12">
+		<iframe scrolling="no" style="border:none;width:100%;height:1000px;overflow:hidden;" src = '{{ helper()->urlBackend("elfinder") }}'>
+			
+		</iframe>
+		</div>
+	</div>
+
+@stop
