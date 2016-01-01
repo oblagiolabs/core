5b6bab583134ff5e0928aa5d192758918bff219b
unknown <reza.wikrama3@gmail.com>
2016-01-01 11:31:52 -0800

    displaying oblagio file in view

--- resources/views/backend/crud/form.blade.php
+++ resources/views/backend/crud/form.blade.php
@@ -29,11 +29,7 @@
                     <div class="form-group">
                         
                           <label>Image</label><br/>
-                          <div id = 'image_display'>
-                            <img src= "{{ helper()->assetUrl() }}oblagio/images/image.png" />
-                          </div>
-                          {!! Form::hidden('image' , null , ['class' => 'form-control' , 'style' => 'width:60%;']) !!}
-                          <a data-toggle="modal" data-target="#elfinder_modal" style = 'cursor:pointer;' onclick = 'addElfinder()'>Browse</a>  
+                          {!! html()->oblagioFile('image' , ['name' => 'image']) !!}  
                     </div>
 
                   </div><!-- /.box-body -->
@@ -54,33 +50,5 @@
 
   <div class="row">
       
-  <div class="col-md-12">
-    <div class="modal fade" id="elfinder_modal" role="dialog" >
-      <div class="modal-lg">
-      
-        <!-- Modal content-->
-        <div class="modal-content" style="width:100%;margin-left:30%;"> 
-          <div class="modal-header">
-            <button type="button" id = 'elfinder_close' class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Libraries</h4>
-          </div>
-          <div class="modal-body">
-              
-              <iframe scrolling="no" style="border:none;width:100%;height:450px;overflow:hidden;" src = '{{ helper()->urlBackend("popup-elfinder") }}'>
-        
-              </iframe>
-
-
-
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-        
-      </div>
-    </div>
-  </div>
-  </div>
-
+  
 @stop
